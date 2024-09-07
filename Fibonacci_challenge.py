f5d2ff44b1cd890f617c81924b061474d96694c3
Mawada Sweis <mawada.sweis19@gmail.com>
2024-09-06 21:41:53 -0700

    Add simple test instead of input method

--- Fibonacci_challenge.py
+++ Fibonacci_challenge.py
@@ -4,6 +4,14 @@ def fibonacci(number):
   else:
     return fibonacci(number - 1) + fibonacci(number - 2)
 
-user_input = int(input())
-fibonacci_result = fibonacci(user_input)
-print(f'result = {fibonacci_result}')
+# Test 1: Fibonacci of 2 is 1
+# Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1
+print(f'result = {fibonacci(2)}')
+
+# Test 1: Fibonacci of 3 is 2
+# Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2
+print(f'result = {fibonacci(3)}')
+
+# Test 1: Fibonacci of 4 is 3
+# Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3
+print(f'result = {fibonacci(4)}')
