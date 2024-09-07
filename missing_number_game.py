c047620856b76eca75bf972904ee5210861f3cdf
Mawada Sweis <mawada.sweis19@gmail.com>
2024-09-06 21:45:21 -0700

    Add test input instead of user input method

--- missing_number_game.py
+++ missing_number_game.py
@@ -3,5 +3,13 @@ def missing_number(numbers):
     if num not in numbers:
       return num
 
-nums_input = list(map(int, input().split(',')))
-print(f'missing number is: {missing_number(nums_input)}')
+# Test 1: 
+# all numbers are in the range [0,3]. 
+# 2 is the missing number in the range since it does not appear in the list
+print(f'missing number is: {missing_number([3,0,1])}')
+
+# Test 2: 
+print(f'missing number is: {missing_number([0,1])}')
+
+# Test 3: 
+print(f'missing number is: {missing_number([9,6,4,2,3,5,7,0,1])}')
